160b57d9969748822b8d7cd37a69f0be878fb8a6
sylvainnizac <sylvain.nizac@laposte.net>
2017-03-22 00:16:45 +0100

    nicer construction & 404 pages

--- pages/pages.py
+++ pages/pages.py
@@ -4,9 +4,15 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('static'))
 skeleton = env.get_template('/templates/skeleton.html')
 
+
 def error_404(status, message, traceback, version):
     template = env.get_template('/pages/404.html')
-    return skeleton.render(content = template.render(message = message), title = "404... Oups!")
+    kwargs = {
+        "content": template.render(message = message),
+        "title": "404... Oups!"
+        }
+    return skeleton.render(**kwargs)
+
 
 class StaticPages(object):
 
@@ -20,4 +26,8 @@ class StaticPages(object):
     @cp.expose
     def CV(self):
         template = env.get_template('/pages/CV.html')
-        return self.skeleton.render(content = template.render(), title = "CV NIZAC Sylvain")
+        kwargs = {
+            "content": template.render(),
+            "title": "CV NIZAC Sylvain"
+            }
+        return self.skeleton.render(**kwargs)
