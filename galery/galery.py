72f65543c0e1a0bfcf74757226594d5f1640c886
Sylvain NIZAC <sylvain.nizac@laposte.net>
2017-03-28 10:31:40 +0200

    Create galery.py

---
+++ galery/galery.py
@@ -0,0 +1,16 @@
+import cherrypy as cp
+
+from config import skeleton, under_construction
+
+
+class Root(object):
+
+    @cp.expose
+    def index(self):
+        template = under_construction  # scafold
+        rendered_template = template.render()
+        kwargs = {
+            "content": rendered_template,
+            "title": "page en construction"
+            }
+        return skeleton.render(**kwargs)
