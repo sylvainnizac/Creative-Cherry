3fed547b1cc73fa8ba6ab7082b3af17b0d3178a9
sylvainnizac <sylvain.nizac@laposte.net>
2017-03-30 22:42:09 +0200

    update requierements

--- config.py
+++ config.py
@@ -7,18 +7,7 @@ skeleton = env.get_template('/templates/skeleton.html')
 under_construction = env.get_template('/pages/under_construction.html')
 
 
-root_conf = {
-       '/': {
-            'tools.sessions.on': True,
-            'tools.staticdir.root': os.path.abspath(os.getcwd())
-        },
-       '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': './static'
-        }
-}
-
-st_pages_conf = {
+common_conf = {
        '/': {
             'tools.sessions.on': True,
             'tools.staticdir.root': os.path.abspath(os.getcwd())
