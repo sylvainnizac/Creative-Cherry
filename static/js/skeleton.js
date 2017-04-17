01402d8216b0c4f75723197a695c49866ddcb702
sylvainnizac <sylvain.nizac@laposte.net>
2017-04-17 11:46:51 +0200

    update navbar functions

--- static/js/skeleton.js
+++ static/js/skeleton.js
@@ -1,9 +1,14 @@
-setTimeout(function() {
-    $('#common-navbar').slideUp(500);
-    $('#nav_bar_appear_button').slideDown(500);
-}, 3000);
-
 function changeVisibility() {
     $('#nav_bar_appear_button').slideUp('fast');
     $('#common-navbar').slideDown('fast');
+    AutoHide(5000);
 }
+
+function AutoHide(dtime) {
+    setTimeout(function() {
+        $('#common-navbar').slideUp(500);
+        $('#nav_bar_appear_button').slideDown(500);
+    }, dtime);
+}
+
+AutoHide(3000);
