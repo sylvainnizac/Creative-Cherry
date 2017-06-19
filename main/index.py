1b21c77c5e624db3cb975c72a4296a03ba10854e
Sylvain NIZAC <sylvain.nizac@laposte.net>
2017-06-19 16:36:03 +0200

    Update index.py

--- main/index.py
+++ main/index.py
@@ -1,16 +1,18 @@
 import cherrypy as cp
 
-from config import skeleton, under_construction
+from config import skeleton, under_construction, main
 
 
 class Root(object):
 
     @cp.expose
     def index(self):
-        template = under_construction  # scafold
-        rendered_template = template.render()
+        #template = under_construction  # scafold
+        page_content = {"data": ""}
+        template = main
+        rendered_template = template.render(**page_content)
         kwargs = {
             "content": rendered_template,
-            "title": "page en construction"
+            "title": "Accueil"
             }
         return skeleton.render(**kwargs)
